20375f94beb07f252bacae8834e8940a7ef78914
ALeksandraLev <sasha.levashova@mail.ru>
2023-10-16 13:53:50 +0300

    Added Audio
    Added Audio (initial voiceover). Changed the location of the button for the information panel and the information panel.

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    bool isOn = false;
+    [SerializeField] GameObject Play_Button;
+    [SerializeField] GameObject Pause_Button;
+    public void Play_Button_Button_Click()
+    {
+        if (isOn) //кнопка нажата
+        {
+            Pause_Button.SetActive(false);
+            Play_Button.SetActive(true);
+            isOn = false;
+        }
+        else //кнопка не нажата
+        {
+            Pause_Button.SetActive(true);
+            Play_Button.SetActive(false);
+            isOn = true;
+        }
+    }
+}
