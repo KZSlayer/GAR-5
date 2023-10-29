708f4f68c77045b7cb701aabd0a2a5724cdc62a9
KZSlayer <nikitaklimus2002@gmail.com>
2023-10-29 22:25:31 +0300

    Added Stop Button and something else
    Change audio

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -7,17 +7,20 @@ public class Play : MonoBehaviour
     bool isOn = false;
     [SerializeField] GameObject Play_Button;
     [SerializeField] GameObject Pause_Button;
+    [SerializeField] GameObject Stop_Button;
     public void Play_Button_Button_Click()
     {
         if (isOn) //кнопка нажата
         {
             Pause_Button.SetActive(false);
+            //Stop_Button.SetActive(false);
             Play_Button.SetActive(true);
             isOn = false;
         }
         else //кнопка не нажата
         {
             Pause_Button.SetActive(true);
+            Stop_Button.SetActive(true);
             Play_Button.SetActive(false);
             isOn = true;
         }
