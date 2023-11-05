dc37b94da9a14fb94ec19392efeb400f31816323
ALeksandraLev <sasha.levashova@mail.ru>
2023-11-05 12:15:04 +0300

    Fixed a bug with the Stop button

--- Assets/Scripts/Play_2.cs
+++ Assets/Scripts/Play_2.cs
@@ -1,30 +1,33 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Play : MonoBehaviour
-//{
-//    bool isOn = false;
-//    bool i = false;
-//    [SerializeField] GameObject Play_Button;
-//    [SerializeField] GameObject Pause_Button;
-//    [SerializeField] GameObject Stop_Button;
-//    public void Play_Button_Click()
-//    {
-//        if (isOn && i) //кнопка нажата
-//        {
-//            Pause_Button.SetActive(false);
-//            //Stop_Button.SetActive(false);
-//            Play_Button.SetActive(true);
-//            isOn = false;
-//            i = true;
-//        }
-//        else //кнопка не нажата
-//        {
-//            Pause_Button.SetActive(true);
-//            Stop_Button.SetActive(true);
-//            Play_Button.SetActive(false);
-//            isOn = true;
-//        }
-//    }
-//}
+public class Play_2 : MonoBehaviour
+{
+    bool isOn = false;
+    bool isSpop = false;
+    [SerializeField] GameObject Play_Button;
+    [SerializeField] GameObject Pause_Button;
+    public void Play_Button_Click()
+    {
+        if (isOn && isSpop == false) //кнопка нажата
+        {
+            Pause_Button.SetActive(false);
+            Play_Button.SetActive(true);
+            isOn = false;
+        }
+        else //кнопка не нажата
+        {
+            Pause_Button.SetActive(true);
+            Play_Button.SetActive(false);
+            isOn = true;
+            isSpop = false;
+        }
+    }
+    public void Stop_Button_Click()
+    {
+        Pause_Button.SetActive(false);
+        Play_Button.SetActive(true);
+        isSpop = true;
+    }
+}
